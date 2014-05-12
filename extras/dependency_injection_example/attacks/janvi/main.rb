f714374c68fb4e591783c4666683603ca69c7295
Janvi Shah <shahjanvi54@gmail.com>
2014-05-12 13:57:23 +0530

    Solution of DI

--- extras/dependency_injection_example/attacks/janvi/main.rb
+++ extras/dependency_injection_example/attacks/janvi/main.rb
@@ -4,8 +4,8 @@ require "./gseb_school_extractor"
 
 # Do not change line 8 & 11
 
-cbse_school_printer = SchoolPrinter.new("cbse")
+cbse_school_printer = SchoolPrinter.new(CbseSchoolExtractor.new)
 cbse_school_printer.print_lables_for_all_schools
 
-gseb_school_printer = SchoolPrinter.new("gseb")
+gseb_school_printer = SchoolPrinter.new(GsebSchoolExtractor.new)
 gseb_school_printer.print_lables_for_all_schools
--- extras/dependency_injection_example/attacks/janvi/school_printer.rb
+++ extras/dependency_injection_example/attacks/janvi/school_printer.rb
@@ -1,19 +1,11 @@
 class SchoolPrinter
 
-  def initialize(mode)
-    @mode = mode
+  def initialize(extractor)
+    @extractor = extractor
   end
 
   def print_lables_for_all_schools
-
-    if @mode == "cbse"
-      cbse = CbseSchoolExtractor.new
-      puts cbse.get_all_schools
-    elsif @mode == "gseb"
-      gseb = GsebSchoolExtractor.new
-      puts gseb.get_all_schools
-    end
-      
+    puts @extractor.get_all_schools
   end
 
 end
