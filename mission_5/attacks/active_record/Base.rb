ccf475155c176d8de285d9492672b07bebf7ed2a
Janvi Shah <shahjanvi54@gmail.com>
2014-05-20 16:16:22 +0530

    all method work for all class

--- mission_5/attacks/active_record/Base.rb
+++ mission_5/attacks/active_record/Base.rb
@@ -8,40 +8,29 @@ module ActiveRecord
     def self.attr_accessor(*vars)
       @attributes ||= []
       @attributes.concat vars
-      super(*vars)
     end
 
     def self.attributes
       @attributes
     end
-
-    def attributes
+   
+    def attributes     
       self.class.attributes
     end
 
-    def self.all
-      # get model name
-      puts self.name
-      
-      #get table name
-      puts self.name.pluralize
-      
+    def self.all      
       #get csv file in the class
-      name = self.name.pluralize.downcase            
-    
+      name = self.name.pluralize.downcase                  
+
       i = 0;         
       @data = []
       CSV.foreach("./database/#{name}.csv") do |row|
         col = row.length
         f = self.new
         (0..col-1).each_with_index do |col_num,index|
-          if i != 0                   
-            f.attributes[index] = row[index]           
-          end           
+          f.attributes[index] = row[index] if i != 0                                     
         end   
-        if i>0  
-          @data.push(f)          
-        end
+        @data.push(f) if i>0  
         i = i + 1
       end      
       @data
