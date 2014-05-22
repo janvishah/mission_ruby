bf57e373b77676d2050aa8916eeb7a2ba17a1769
Janvi Shah <shahjanvi54@gmail.com>
2014-05-22 12:06:49 +0530

    Find_by method create

--- mission_5/attacks/active_record/Base.rb
+++ mission_5/attacks/active_record/Base.rb
@@ -19,16 +19,42 @@ module ActiveRecord
             attr_names.push(row[col_num].strip)
           end
         else   
-          attr_names.each_with_index do |attr_name,index|  
-            puts attr_name                    
+          attr_names.each_with_index do |attr_name,index|                              
             f.send("#{attr_name}=",row[index])
           end
         end                    
-        data.push(f) if i>0                
-        end
+        data.push(f) if i>0                       
         i = i + 1
       end          
       data      
     end
+
+    def self.find_by(var)
+      name = self.name.pluralize.downcase
+      i = 0
+      place_of_attribute = nil
+      key = var.keys[0]
+      value = var.values[0]
+      attr_names = []
+      CSV.foreach("./database/#{name}.csv") do |row|
+        s = self.new
+        col = row.length        
+        if i == 0
+          (0..col-1).each do |col_num|                    
+            place_of_attribute = col_num if row[col_num].strip == key.to_s           
+            attr_names.push(row[col_num].strip)        
+          end        
+        else
+          if row[place_of_attribute].strip == value.to_s           
+            attr_names.each_with_index do |attr_name,index|                              
+              s.send("#{attr_name}=",row[index].strip)
+            end
+            return s
+          end
+        end
+        place_of_attribute != nil ? i += 1 : break      
+      end 
+    end
+    
   end
 end
--- mission_5/attacks/main.rb
+++ mission_5/attacks/main.rb
@@ -1,5 +1,5 @@
 require "csv"
-require "./active_record/test"
+require "./active_record/Base"
 include ActiveRecord
 require "./City"
 require "./State"
@@ -9,3 +9,16 @@ require "./Country"
 puts Country.all.count == 2 # Returns array of all countries.
 puts State.all.count == 4   # Returns array of all states.
 puts City.all.count == 8    # Returns array of all cities.
+
+country = Country.find_by name: 'india'
+puts country.name == "india"
+country = Country.find_by id: 2         # Country object.
+puts country.name == "pakistan"
+state = State.find_by name: 'gujarat'   # State object.
+puts state.name == "gujarat"
+state = State.find_by id: 2             # State object.
+puts state.name == "maharashtra"
+city = City.find_by name: 'bharuch'     # City object.
+puts city.name == "bharuch"
+city = City.find_by id: 4               # City object.
+puts city.name == "pune"
